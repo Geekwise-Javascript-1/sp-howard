47008c96d8c90ec2acfef30acf6906ab54783b1b
Steven Howard <stevenphoward@gmail.com>
2016-06-29 00:17:23 -0700

    Assignment 1 completed, I think

---
+++ app01.js
@@ -0,0 +1,16 @@
+/*
+Steven Howard
+6/28/16
+Javascript 1
+*/
+
+var dec12 = 12;
+var hex30 = "0x"+(30).toString(16);
+
+var string = "Answer equals: ";
+
+var combined = dec12 + parseInt(hex30);
+
+var answer = string + combined;
+
+console.log(answer);
